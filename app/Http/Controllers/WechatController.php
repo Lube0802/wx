340e6dcad977975cd41ee9ffa759405b4ea94c55
liuzhen <you@example.com>
2018-08-01 17:13:07 +0800

    test

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use EasyWeChat\Foundation\Application;
 use Helper;
 
 class WechatController extends BaseController
 {
     public function index()
     {
-        
+        $app = new Application(config('wechat'));
+        return $app->server->serve();
     }
 }
